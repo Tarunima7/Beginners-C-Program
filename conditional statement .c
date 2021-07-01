6f3ae8fe92c92b971f9100ab75ac663a114be78e
TARUNIMA SHARMA <77146630+Tarunima7@users.noreply.github.com>
2021-07-02 01:27:48 +0530

    Create conditional statement .c

---
+++ conditional statement .c
@@ -0,0 +1,10 @@
+/*
+The operations specified in if block are executed if and only if the given condition is true. Otherwise, it will get skipped.
+
+There are four different types of if statement in C. These are:
+
+Simple if Statement
+if-else Statement
+Nested if-else Statement
+else-if Ladder
+*/
